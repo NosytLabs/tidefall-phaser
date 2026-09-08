2b012521ab1026c93c3a333c7a439f611a60b8dc
Nosyt Labs <hi@nosytlabs.com>
2026-09-07 23:57:54 -0300

    test: update HUD file

--- src/scenes/UISceneV2.js
+++ src/scenes/UISceneV2.js
@@ -1,2 +1,2 @@
 import Phaser from 'phaser';
-export class UISceneV2 extends Phaser.Scene { constructor(){ super({key:'UIScene',active:false}); } create(){ this.add.text(10,10,'TIDEFALL'); } }
+export class UISceneV2 extends Phaser.Scene { constructor(){ super({key:'UIScene',active:false}); } create(){ this.add.text(8,8,'TIDEFALL'); } }
